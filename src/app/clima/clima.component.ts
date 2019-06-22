5e8142d8be04b4c151139bfececc8fb138fad965
Andres0510 <j.andres0510@hotmail.com>
2019-06-22 09:58:17 -0500

    Dojo angular

---
+++ src/app/clima/clima.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http"
+
+@Component({
+  selector: 'app-clima',
+  templateUrl: './clima.component.html',
+  styleUrls: ['./clima.component.css']
+})
+export class ClimaComponent implements OnInit {
+
+  private resultado=null;
+  constructor(private http:HttpClient) { }
+
+
+  ngOnInit() {
+    this.http.get("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=bf8fe02fb5288ea367500fa2eab2fd09").subscribe(
+      result=>{
+        this.resultado=result;
+      },
+      error=>{
+        console.log("Error");
+      }
+    )
+  }
+
+  consultar(ciudad){
+    this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad.value},uk&APPID=bf8fe02fb5288ea367500fa2eab2fd09`).subscribe(
+      result=>{
+        this.resultado=result;
+      },
+      error=>{
+        console.log("Error");
+      }
+    )
+  }
+
+}
---
+++ src/app/estudiante/estudiante.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { RecursiveTemplateAstVisitor } from '@angular/compiler';
+
+@Component({
+  selector: 'app-estudiante',
+  templateUrl: './estudiante.component.html',
+  styleUrls: ['./estudiante.component.css']
+})
+export class EstudianteComponent implements OnInit {
+
+  @Input() receptor:string;
+  vector=[1,2,3,4];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
